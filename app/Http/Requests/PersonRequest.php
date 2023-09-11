31ffd84da4d1cb007a2fb14cc233f82fd63b9b5c
Oluwakayode Adegbuyi <kayode_adegbuyi@yahoo.com>
2023-09-11 16:32:17 +0100

    added validation

--- app/Http/Requests/PersonRequest.php
+++ app/Http/Requests/PersonRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class PersonRequest extends FormRequest
+class PersonRequest extends BaseFormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
